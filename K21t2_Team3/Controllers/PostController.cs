3937da9dfe5743a52cead8626a1b4db525289096
T152842 - MAI HUYNH NAM - K21T02 <nammai3@vanlanguni.vn>
2017-11-18 02:29:34 +0700

    Thiết kế giao diện cho chức năng post project và thực hiện một số tính năng

---
+++ K21t2_Team3/Controllers/PostController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using K21t2_Team3.Models;
+using K21t2_Team3.ViewModels;
+
+namespace K21t2_Team3.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        DemoPPCRentalEntities entities = new DemoPPCRentalEntities();
+        public ActionResult Post()
+        {
+            var vm = new ViewModel();
+            vm.zDistricts = entities.DISTRICTs.ToList();
+            vm.zStreets = entities.STREETs.ToList();
+            vm.zWards = entities.WARDs.ToList();
+            return View(vm);
+        }
+        public string ProcessUpload(HttpPostedFileBase file)
+        {
+
+            file.SaveAs(Server.MapPath("/Images/" + file.FileName));
+            return " " +file.FileName;
+        }
+    }
+
+}
